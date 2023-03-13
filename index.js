47687a9c791088f1adb66dcdbbf54c1fa213c753
skudinva <skudinva@gmail.com>
2023-03-13 12:28:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import Post from "./Post.js";
 import router from "./router.js";
+import fileUpload from 'express-fileupload';
 
 const PORT = 5000;
 //const DB_URL = 'mongodb://localhost:27017/backend?retryWrites=true&w=majority'
@@ -8,6 +9,8 @@ const PORT = 5000;
 
 const app = express()
 app.use(express.json())
+app.use(express.static('static'))
+app.use(fileUpload())
 app.use('/api', router)
 
 async function startApp(){
